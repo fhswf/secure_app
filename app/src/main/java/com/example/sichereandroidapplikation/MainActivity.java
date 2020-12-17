94fc62cb3dacf60e4a68be8e7f950a673f5937e9
Marcel Hopp <m94_hopp@web.de>
2020-12-17 16:37:31 +0100

    Added NetworkInfo

--- app/src/main/java/com/example/sichereandroidapplikation/MainActivity.java
+++ app/src/main/java/com/example/sichereandroidapplikation/MainActivity.java
@@ -15,6 +15,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +26,8 @@ import com.auth0.android.Auth0;
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private Auth0 auth0;
+
+    NetworkChangeListener networkChangeListener = new NetworkChangeListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,4 +57,16 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    protected void onStart() {
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkChangeListener, filter);
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        unregisterReceiver(networkChangeListener);
+        super.onStop();
+    }
 }
