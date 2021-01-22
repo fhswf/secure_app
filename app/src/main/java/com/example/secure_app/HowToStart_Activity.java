954e37ba78738cb74d647244dd069217e730f9a3
Marcel Hopp <m94_hopp@web.de>
2021-01-22 11:28:31 +0100

    Anpassung Schriftfarbe und Text HowToStart_Activity

--- app/src/main/java/com/example/secure_app/HowToStart_Activity.java
+++ app/src/main/java/com/example/secure_app/HowToStart_Activity.java
@@ -1,15 +1,19 @@
-/**
- * Diese Activity implementiert Hinweise zu den ersten Schritten in dieser Applikation.
- *
- * @author Marcel Hopp
- */
 package com.example.secure_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
+/**
+ * Diese Activity implementiert Hinweise zu den ersten Schritten in dieser Applikation.
+ *
+ * @author Marcel Hopp
+ */
 public class HowToStart_Activity extends AppCompatActivity {
+
+    TextView link_github;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,8 @@ public class HowToStart_Activity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("How to Start?");
+
+        link_github = (TextView) findViewById(R.id.text_how_to_start);
+        link_github.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
